48d6edab0f12996f143d13fba3029b9d1bac0b51
milleniasaharani <milleniashrn@gmail.com>
2020-01-15 19:47:54 +0700

    Second commit

--- src/Belajar.java
+++ src/Belajar.java
@@ -2,6 +2,7 @@ public class Belajar {
 
     public static void main(String[] args) {
         System.out.println("Learn to build automatic project on Jenkins");
+	System.out.println("First change");
     }
     
 }
